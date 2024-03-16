d733ddbc2aded0f8fa83a0bf9560bd1ef44fa98d
prateek0411999 <prateekshrmprateek@gmail.com>
2024-03-16 20:36:03 +0530

    added test - single number

--- string_convertor_spec.rb
+++ string_convertor_spec.rb
@@ -12,5 +12,10 @@ RSpec.describe "StringConvertor" do
               expect(StringConvertor.convert("")).to eq(0)
             end
         end
+        context "when input is a single number" do
+            it "should return the number" do
+              expect(StringConvertor.convert("1")).to eq(1)
+            end
+        end
     end
 end
