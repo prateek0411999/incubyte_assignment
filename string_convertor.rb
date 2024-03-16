91252563fc1067ba313cd1b90f29168a85ba6b96
prateek0411999 <prateekshrmprateek@gmail.com>
2024-03-16 22:02:47 +0530

    modified method - check negative numbers

--- string_convertor.rb
+++ string_convertor.rb
@@ -14,7 +14,12 @@ class StringConvertor
         
         numbers = numbers.gsub("\n", delimeter)
         number_list = numbers.split(delimeter)
-      
+
+        negatives = number_list.select { |num| num.to_i < 0 }.map(&:to_i)
+        if negatives.any?
+          raise "negative numbers not allowed #{negatives.join(',')}"
+        end
+
         return number_list.map(&:to_i).sum
     end
 end
