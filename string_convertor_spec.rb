3011d050f56adfa8ae42b3c20c4cfdb2acf7598f
prateek0411999 <prateekshrmprateek@gmail.com>
2024-03-16 20:28:54 +0530

    added test case - respond_to convert method

--- string_convertor_spec.rb
+++ string_convertor_spec.rb
@@ -1,1 +1,11 @@
 require_relative "./string_convertor.rb"
+
+RSpec.describe "StringConvertor" do 
+    describe ".convert" do 
+        context "convert class_method is present" do 
+            it "should respond_to 'convert' method" do 
+                expect(StringConvertor).to respond_to(:convert)
+            end 
+        end 
+    end 
+end 
