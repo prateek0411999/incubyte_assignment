a72e24c3d2eaffda197a1e93c8e2205227325ef1
prateek0411999 <prateekshrmprateek@gmail.com>
2024-04-24 17:00:08 +0530

    restructed convert method

--- string_convertor.rb
+++ string_convertor.rb
@@ -1,8 +1,19 @@
 require "byebug"
 class StringConvertor
     def self.convert(numbers = nil)
-        return 0 if numbers.empty?
+        begin
+            return 0 if numbers.empty?
 
+            delimeter = fetch_delimiter(numbers)
+            number_list = build_number_list(delimeter, numbers)
+            check_for_negatives(number_list)
+            return number_list.map(&:to_i).sum
+        rescue Exception => e
+            raise e.message
+        end
+    end
+
+    def self.fetch_delimiter(numbers)
         delimeter = ","
         updated_delimeter = nil
         if numbers.start_with?("//")
@@ -11,15 +22,20 @@ class StringConvertor
             numbers = match[2]
         end
         delimeter = updated_delimeter if !updated_delimeter.nil?
-        
+        return delimeter
+    end
+
+    def self.build_number_list(delimeter, numbers)
         numbers = numbers.gsub("\n", delimeter)
         number_list = numbers.split(delimeter)
+        number_list
+    end
 
+    def self.check_for_negatives(number_list)
         negatives = number_list.select { |num| num.to_i < 0 }.map(&:to_i)
         if negatives.any?
-          raise "negative numbers not allowed #{negatives.join(',')}"
+            raise "negative numbers not allowed #{negatives.join(',')}" 
         end
 
-        return number_list.map(&:to_i).sum
     end
 end
